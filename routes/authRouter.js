e68945b1a166b7bdeb77c21c13a99b8892875570
Jinho <breezjin@gmail.com>
2022-03-11 09:38:46 +0900

    pipe 수정

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -5,6 +5,7 @@ const { signinKakao, sendVerified } = require('../controllers/authController');
 const verifyBreezToken = require('../middlewares/verifyToken');
 
 router.post('/kakao', signinKakao);
+router.post('/naver');
 router.post('/token-verification', verifyBreezToken, sendVerified);
 
 module.exports = router;
